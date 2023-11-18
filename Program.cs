c6c0a3a191c1d35b39797a9d004484d8d5c8492c
giorgi mikeladze <mikeladzegiorgi538@outlook.com>
2023-11-18 18:32:45 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.Write("Enter an integer value: ");
+bool isParsed = int.TryParse(Console.ReadLine(), out int numb);
+
+if (isParsed && numb >= 1)
+{
+    var binaryList = new List<int>();
+
+    while (numb > 0)
+    {
+        binaryList.Add(numb % 2);
+        numb /= 2;
+    }
+
+    Console.Write("Binary representation: ");
+    for (int i = binaryList.Count - 1; i >= 0; i--)
+    {
+        Console.Write(binaryList[i]);
+    }
+}
+else
+{
+    Console.WriteLine("Invalid value! Please enter a positive integer.");
+}
